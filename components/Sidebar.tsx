b343d156659a082cf382edc2e13e202a6a01f9b2
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-12-16 17:46:13 -0500

    sidebar done
    sidebar done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,20 +16,20 @@ function Sidebar() {
         className="w-32 h-32 mx-auto rounded-full"
       />
       <h3 className="my-4 text-3xl font-medium tracking-wider font-oswald">
-        <span>Matthew </span>
+        <span className="text-green">Matthew </span>
         Evans
       </h3>
       <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Web Developer</p>
       <a
         href=""
         download="name"
-        className="px-2 py-1 my-3 bg-gray-200 rounded-full flex items-center justify-center"
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
       >
         <GiTie className="w-6 h-6" />
         Download Resume
       </a>
       {/* //social icons */}
-      <div className="flex justify-around my-5 text-green-500 w-9/12 mx-auto md:w-full">
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
         <a href="">
           <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
@@ -42,7 +42,7 @@ function Sidebar() {
       </div>
 
       {/* address */}
-      <div className="my-5 py-4 bg-gray-200">
+      <div className="py-4 my-5 bg-gray-200">
         <div className="flex items-center justify-center space-x-2">
           <GoLocation className="w-6 h-6" />
           <span>Irwin, PA</span>
@@ -51,10 +51,13 @@ function Sidebar() {
         <p className="my-2">724-771-1111</p>
       </div>
       {/* //email button */}
-      <button className=" bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">
+      <button
+        className="w-8/12 px-5 py-2 my-2 text-white rounded-full  bg-gradient-to-r from-green to-blue-400 focus:outline-none"
+        onClick={() => window.open("mailto:maevans724@gmail.com")}
+      >
         Email Me
       </button>
-      <button className=" bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">
+      <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full  bg-gradient-to-r from-green to-blue-400">
         Light UI
       </button>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6 my-14 lg:px-48 px-5">
-      <div className="col-span-12 bg-white rounded-2xl lg:col-span-3 pd-4 text-center">
+    <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:px-48 sm:px-20 md:px-32">
+      <div className="col-span-12 text-center bg-white rounded-2xl lg:col-span-3 pd-4">
         <Sidebar />
       </div>
       <div className="col-span-12 bg-white rounded-2xl lg:col-start-9">
